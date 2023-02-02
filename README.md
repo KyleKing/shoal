77d95f47d8076a1baa333f242c220bdfa5d1666b
Kyle King <KyleKing@users.noreply.github.com>
2023-02-01 20:55:41 -0500

    feat: initialize with copier

---
+++ README.md
@@ -0,0 +1,5 @@
+# shoal
+
+Lazy, Python-first, alternative to make, just, doit, and invoke
+
+Documentation can be found on [Github (./docs)](./docs), [PyPi](https://pypi.org/project/shoal/), or [Hosted](https://shoal.kyleking.me/)!
---
+++ shoal/__init__.py
@@ -0,0 +1,10 @@
+"""shoal."""
+
+from loguru import logger
+
+__version__ = '0.0.1'
+__pkg_name__ = 'shoal'
+
+logger.disable(__pkg_name__)
+
+# ====== Above is the recommended code from calcipy_template and may be updated on new releases ======

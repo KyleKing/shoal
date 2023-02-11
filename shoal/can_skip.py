a120a78136f7222b35c811e4e52faa49d67cc7bb
Kyle King <KyleKing@users.noreply.github.com>
2023-02-11 16:58:27 -0500

    refactor: use prerequisites for consistency

--- shoal/can_skip.py
+++ shoal/can_skip.py
@@ -7,28 +7,28 @@ from beartype.typing import List
 
 
 @beartype
-def can_skip(*, depends: List[Path], targets: List[Path]) -> bool:
-    """Generic make-style task skipping logic based on file mtime.
+def can_skip(*, prerequisites: List[Path], targets: List[Path]) -> bool:
+    """Generic make-style task skipping logic based on file `mtime`.
 
     Example use with Invoke, but can be used anywhere:
 
     ```py
     @task
     def test(ctx: Context) -> None:
-        if can_skip(depends=[*Path('src').rglob('*.py')], targets=[Path('.coverage.xml')]):
+        if can_skip(prerequisites=[*Path('src').rglob('*.py')], targets=[Path('.coverage.xml')]):
             return  # Exit early
 
         ...  # Task code
     ```
 
     """
-    ts_depends = [pth.getmtime() for pth in depends]
-    if not ts_depends:
-        raise ValueError('Required files do not exist', depends)
+    ts_prerequisites = [pth.getmtime() for pth in prerequisites]
+    if not ts_prerequisites:
+        raise ValueError('Required files do not exist', prerequisites)
 
     # TODO: Triple check this logic (https://stackoverflow.com/a/22960700/3219667)
     ts_targets = [pth.getmtime() for pth in targets]
-    if ts_targets and max(ts_depends) >= min(ts_targets):
+    if ts_targets and max(ts_prerequisites) >= min(ts_targets):
         logger.info('Skipping because targets are newer', targets=targets)
         return False
     return True

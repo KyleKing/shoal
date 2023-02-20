2c3cc1dc7deef5f564153757da35fe80681c0599
Kyle King <KyleKing@users.noreply.github.com>
2023-02-20 07:34:41 -0500

    feat: add logger.print and print_debug

--- shoal/log.py
+++ shoal/log.py
@@ -5,7 +5,7 @@ import logging
 from functools import cached_property, partial
 
 from beartype import beartype
-from beartype.typing import Any, Callable, Dict
+from beartype.typing import Any, Callable, Dict, Optional
 from pydantic import BaseModel
 from rich.console import Console
 from rich.text import Text
@@ -24,7 +24,7 @@ class _Styles:
     level_info: str = 'green'
     level_debug: str = 'dim blue'
 
-    key: str = 'green'
+    key: str = 'bold blue'
     value: str = ''
 
     @cached_property
@@ -55,23 +55,35 @@ _STYLES = _Styles()
 
 
 @beartype
-def _log(message, *, _log_level: int, _this_level: int, _console: Console, **kwargs) -> None:
+def _log(
+    message,
+    *,
+    _log_level: int, _this_level: int, _console: Console,
+    is_print: bool = False, is_header: bool = False,
+    **kwargs,
+) -> None:
     """Default log function."""
-    if _this_level >= _log_level:
-        text = Text()
-        text.append(f"{datetime.now()} ", style=_STYLES.timestamp)
-        text.append(_LEVEL_TO_NAME.get(_this_level), style=_STYLES._level_lookup.get(_this_level))
-        text.append(f" {message}", style=_STYLES.message)
-        for key, value in kwargs.items():
-            text.append(f' {key}:', style=_STYLES.key)
-            text.append(f' {str(value): <10}', style=_STYLES.value)
-        _console.print(text)
+    if _this_level < _log_level:
+        return
 
-        if _this_level == logging.CRITICAL:
-            _console.print_exception(show_locals=True)
-            # # Or:
-            # from rich.traceback import install
-            # install(show_locals=True)
+    text = Text()
+    if is_print:
+        mesage_style = ('bold ' if is_header else '') + _STYLES.level_info
+        text.append(f"{message}", style=mesage_style)
+    else:
+        text.append(f"{datetime.now()} ", style=_STYLES.timestamp)
+        text.append(_LEVEL_TO_NAME.get(_this_level, ''), style=_STYLES._level_lookup.get(_this_level))
+        text.append(f" {message}", style=_STYLES.message)
+    for key, value in kwargs.items():
+        text.append(f' {key}:', style=_STYLES.key)
+        text.append(f' {str(value): <10}', style=_STYLES.value)
+    _console.print(text)
+
+    if _this_level == logging.CRITICAL:
+        _console.print_exception(show_locals=True)
+        # # Or:
+        # from rich.traceback import install
+        # install(show_locals=True)
 
 
 class _LogSingleton(BaseModel):
@@ -84,6 +96,20 @@ _LOG_SINGLETON = _LogSingleton(log=partial(_log, _log_level=_DEF_LEVEL, _console
 
 
 class _Logger:
+
+    @beartype
+    def print(self, message, is_header: bool = False, **kwargs) -> None:
+        """Print the content without a leading timestamp.
+
+        If writing to a file or not natively supported by the logger, will appear in the logs as level info.
+
+        """
+        self.info(message, **{'is_print': True, 'is_header': is_header, **kwargs})
+
+    @beartype
+    def print_debug(self, message, is_header: bool = False, **kwargs) -> None:
+        """Variation on print that will appear as a debug log if not supported."""
+        self.debug(message, **{'is_print': True, 'is_header': is_header, **kwargs})
 
     @beartype
     def debug(self, message, **kwargs) -> None:
